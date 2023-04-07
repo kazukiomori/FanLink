3f2e4204aebc29f012655da0dba5d68897a98d68
kazukiomori <kazuki1110170501@gmail.com>
2023-04-07 23:56:24 +0900

    ライフサイクルを追加

--- FanLink/ViewController/ViewController.swift
+++ FanLink/ViewController/ViewController.swift
@@ -9,11 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
 
 }
 
